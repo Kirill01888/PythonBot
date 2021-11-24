dfec5c6bb688a28aae2b2d331867f484c6fe0af7
Kirilon01888 <petrisevkirill144@gmail.com>
2021-11-25 01:04:16 +0300

    split and strip words

--- discord-bot.py
+++ discord-bot.py
@@ -6,14 +6,25 @@ def generate():
     str = random.choice(part1Text) + random.choice(part2Text) + random.choice(part3Text) + random.choice(part4Text)
     print (str)
 
+def split_strip(message):
+    m = message.split()
+    list = []
+    for msg in m:
+        str = msg
+        str = str.strip()
+        list.append(str)
+    return list
+
 client = discord.Client()
 
-part1Text = ['Герои пришли в ',"Герои ушли от "]
-part2Text = ["таверны ","шахты "]
-part3Text = ["и перед ними открылся странный пейзаж ","и почуствовали облегчение "]
-part4Text = ["но они хотели отдохнуть.","но они устали."]
+part1Text = ['Герои пришли в ', "Герои ушли от "]
+part2Text = ["таверны ", "шахты "]
+part3Text = ["и перед ними открылся странный пейзаж ", "и почуствовали облегчение "]
+part4Text = ["но они хотели отдохнуть.", "но они устали."]
 
-helloWords = ['hi','hello']
+helloWords = ["hi", "hello",'privet','привет']
+byeWords = ['bye','goodbye','пока','poka']
+helpWords = ['help','commands','помощь','команды']
 
 @client.event
 async def on_ready():
@@ -22,22 +33,27 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
     if message.author == client.user:
-        return await message.channel.send('Hello')
+        return
 
     msg = message.content.lower()
-    msg_list = msg.split
 
-    if len(list(set(msg_list + helloWords))) < len(msg_list) + len(helloWords):
-        await message.channel.send('Hello')
+    listHello = split_strip(msg)
+    listHelp = split_strip(msg)
+    listBye = split_strip(msg)
 
-    if message.content.startswith('Hello'):
-        await message.channel.send(f'Hello!{message.author.mention}')
+    for i in listHello:
+        if i in helloWords:
+            await message.channel.send(f'Hello!{message.author.mention}')
 
-    if message.content.startswith('Help'):
-        await message.channel.send('Commands:')
+    for o in listHelp:
+        if o in helpWords:
+            await message.channel.send('Commands: ')
 
-    if message.content.startswith('Bye'):
-        await message.channel.send('Bye')
+    for p in listBye:
+        if p in byeWords:
+            await message.channel.send(f'Bye {message.author.mention} good luck for you')
+
 
 client.run('')

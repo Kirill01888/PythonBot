9793d20533f39330327ac8b84de18f2fdfd048bd
ura220 <ura1234569966@gmail.com>
2021-11-26 18:47:05 +0300

    E

--- main.py
+++ main.py
@@ -39,21 +39,21 @@ def split_strip(message):
 client = discord.Client()
 
 # Лист ресурсов
-part1Text_raw = ["Герои пришли в ", "Герои ушли от ", "Герои на правились на "]
+part1Text_raw = ["Герои пришли в ", "Герои ушли от ", "Герои направились на "]
 part2Text_raw = ["таверны ", "шахты ", "север ", "восток ", "юг ", "запад "]
 part3Text_raw = ["и перед ними открылся странный пейзаж, ", "и почуствовали облегчение, "]
 part4Text_raw = ["однако они устали и хотели отдохнуть.", "он их удивил.", "хотя раслабляться было рано."]
 
 helloWords = ["hi", "hello", 'privet', 'привет']
 byeWords = ['bye', 'goodbye', 'пока', 'poka']
-helpWords = ['!help', '!commands', '!помощь', '!команды']
+#helpWords = ['!help', '!commands', '!помощь', '!команды']
 startWords = ['!начать', '!start', '!generate', '!сгенерировать']
 
 # Debug
-@client.event
-async def on_ready():
-    print('Bot is working')
-    print(generate())
+# @client.event
+# async def on_ready():
+#     print('Bot is working')
+#     print(generate())
 
 # Определитель слов (Часть 2)
 @client.event
@@ -64,7 +64,7 @@ async def on_message(message):
     msg = message.content.lower()
 
     listHello = split_strip(msg)
-    listHelp = split_strip(msg)
+    #listHelp = split_strip(msg)
     listBye = split_strip(msg)
     listStart = split_strip(msg)
 
@@ -79,18 +79,20 @@ async def on_message(message):
                 await message.channel.send(f'Hello, {message.author.mention}!')
 
     # Помощь
-    for hp in listHelp:
-        if hp in helpWords:
-            if hp == helpWords[0 or 2]:
-                await message.channel.send("Here's the list of commands: ")
-                await message.channel.send(">!start, !generate - Generate the text")
-                await asyncio.sleep(1)
-                await message.channel.send("||That's all ¯\_(ツ)_/¯||")
-            else:
-                await message.channel.send("Вот список команд: ")
-                await message.channel.send(">!начать, !сгенерировать - Сгенерировать")
-                await asyncio.sleep(1)
-                await message.channel.send("||Это всё ¯\_(ツ)_/¯||")
+    # for p in listHelp:
+    #     if p in helpWords:
+    #         if p == helpWords[0 or 1]:
+    #             await message.channel.send("Here's the list of commands: ")
+    #             await asyncio.sleep(0.5)
+    #             await message.channel.send("> !start, !generate - Generate the text")
+    #             await asyncio.sleep(3)
+    #             await message.channel.send("||That's all ¯\_(ツ)_/¯||")
+    #         else:
+    #             await message.channel.send("Вот список команд: ")
+    #             await asyncio.sleep(0.5)
+    #             await message.channel.send("> !начать, !сгенерировать - Сгенерировать текст")
+    #             await asyncio.sleep(3)
+    #             await message.channel.send("||Это всё ¯\_(ツ)_/¯||")
 
     # Прощание
     for b in listBye:
@@ -107,4 +109,4 @@ async def on_message(message):
         if s in startWords:
             await message.channel.send(generate())
 
-client.run('')
+client.run('OTExMjcwNjcyMDc0OTQwNDM2.YZe88g.j8jglXj-wvxwatHOiX4MU1HdMsw')
